d6456304a71975fda6e3496f7279f8816f54e5b8
Raj Khare <raj.khare.java@gmail.com>
2026-02-03 05:16:50 +0530

    dependent assertion

---
+++ code/testing-java-junit5/src/test/java/com/rajkhare/sfgpetclinic/model/OwnerTest.java
@@ -0,0 +1,26 @@
+package com.rajkhare.sfgpetclinic.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OwnerTest {
+
+    @Test
+    void dependentAssertions() {
+
+        Owner owner = new Owner(1L, "Sia", "Khare");
+        owner.setCity("Hyderabad");
+        owner.setTelephone("5432154321");
+
+        assertAll("Properties Test",
+                () -> assertAll("Person Properties",
+                        () -> assertEquals("Sia", owner.getFirstName(), "First Name Did not Match"),
+                        () -> assertEquals("Khare", owner.getLastName())),
+                () -> assertAll("Owner Properties",
+                        () -> assertEquals("Hyderabad", owner.getCity(), "City Did Not Match"),
+                        () -> assertEquals("5432154321", owner.getTelephone())
+                ));
+
+    }
+}

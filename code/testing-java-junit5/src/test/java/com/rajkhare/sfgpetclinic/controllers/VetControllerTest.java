32519f08e451d679e79c855126ce8b56770e2632
Raj Khare <raj.khare.java@gmail.com>
2026-02-11 05:16:20 +0530

    Assignment for VetController Test

---
+++ code/testing-java-junit5/src/test/java/com/rajkhare/sfgpetclinic/controllers/VetControllerTest.java
@@ -0,0 +1,20 @@
+package com.rajkhare.sfgpetclinic.controllers;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class VetControllerTest {
+
+    VetController vetController;
+
+    @BeforeEach
+    void setUp() {
+        vetController = new VetController();
+    }
+
+    @Test
+    void listVets() {
+    }
+}

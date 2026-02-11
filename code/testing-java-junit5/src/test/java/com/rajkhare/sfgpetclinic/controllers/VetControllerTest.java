c8a44da166d467033cc88dad3a492b09ad8102c9
Raj Khare <raj.khare.java@gmail.com>
2026-02-12 05:03:39 +0530

    assignment soultion

--- code/testing-java-junit5/src/test/java/com/rajkhare/sfgpetclinic/controllers/VetControllerTest.java
+++ code/testing-java-junit5/src/test/java/com/rajkhare/sfgpetclinic/controllers/VetControllerTest.java
@@ -1,20 +1,37 @@
 package com.rajkhare.sfgpetclinic.controllers;
 
+import com.rajkhare.sfgpetclinic.fauxspring.Model;
+import com.rajkhare.sfgpetclinic.fauxspring.ModelMapImpl;
+import com.rajkhare.sfgpetclinic.services.SpecialtyService;
+import com.rajkhare.sfgpetclinic.services.VetService;
+import com.rajkhare.sfgpetclinic.services.map.SpecialityMapService;
+import com.rajkhare.sfgpetclinic.services.map.VetMapService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 class VetControllerTest {
+
+    VetService vetService;
+    SpecialtyService specialtyService;
 
     VetController vetController;
 
     @BeforeEach
     void setUp() {
-        vetController = new VetController();
+        specialtyService = new SpecialityMapService();
+        vetService = new VetMapService(specialtyService);
+
+        vetController = new VetController(vetService);
     }
 
     @Test
     void listVets() {
+        Model model = new ModelMapImpl();
+
+        String view = vetController.listVets(model);
+
+        assertThat("vets/index").isEqualTo(view);
     }
 }

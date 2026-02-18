3828c1827807e1a2fea50291814afeae7d5a0c1a
Raj Khare <raj.khare.java@gmail.com>
2026-02-19 04:59:35 +0530

    JUnit default test method

--- code/testing-java-junit5/src/test/java/com/rajkhare/sfgpetclinic/ControllerTests.java
+++ code/testing-java-junit5/src/test/java/com/rajkhare/sfgpetclinic/ControllerTests.java
@@ -1,7 +1,16 @@
 package com.rajkhare.sfgpetclinic;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.TestInstance;
 
 @Tag("controllers")
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public interface ControllerTests {
+
+    @BeforeAll
+    default void beforeAll() {
+        System.out.println("Let's do something here");
+    }
+
 }
